41937ef4254bafd2b850f2ec16fa66b846bf7234
Alexey Tuzov <sugarfox.dev@gmail.com>
2025-12-01 15:06:12 +0100

    add grayscale shader

--- components/rock/rock.gd
+++ components/rock/rock.gd
@@ -13,6 +13,8 @@ const TYPE_TO_TEXTURE_MAP: Dictionary[RockType, Texture] = {
 	RockType.WRONG: preload("res://components/rock/assets/rocks/wrong.svg"),
 }
 
+const GRAYSCALE_MATERIAL = preload("uid://y7ut0534j58n")
+
 var icon_texture_map: Dictionary[String, Texture] = {}
 
 @onready var icon_texture_rect: TextureRect = %IconTextureRect
@@ -34,6 +36,11 @@ const MAX_VARIANT_INDEX = 2
 		icon_variant_index = clampi(value, 0, MAX_VARIANT_INDEX)
 		_update_icon()
 
+@export var is_grayscale: bool = false :
+	set(value):
+		is_grayscale = value
+		_update_icon_filter()
+
 func _ready() -> void:
 	_preload_images()
 
@@ -47,6 +54,10 @@ func _update_texture() -> void:
 
 func _update_icon() -> void:
 	icon_texture_rect.texture = icon_texture_map["%d-%d" % [icon_index, icon_variant_index]]
+
+func _update_icon_filter() -> void:
+	if is_grayscale: icon_texture_rect.material = GRAYSCALE_MATERIAL
+	else: icon_texture_rect.material = null
 
 func _get_item_path(icon: int, variant: int) -> String:
 	return "res://components/rock/assets/shapes/Shape%d/%s.svg" % [clampi(icon, 0, MAX_ICON_INDEX) + 1, ["Default", "Dotted", "Striped"][clampi(variant, 0, MAX_VARIANT_INDEX)]]
